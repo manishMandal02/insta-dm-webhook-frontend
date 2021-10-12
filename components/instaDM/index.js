bdc06df3d4861321a9d065ab9457358158fa63e7
manishMandal02 <manishmandalj@gmail.com>
2021-10-12 18:16:09 +0530

    backend url updated

--- components/instaDM/index.js
+++ components/instaDM/index.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import FacebookLogin from 'react-facebook-login';
+import socketClient from 'socket.io-client';
+
+// const SERVER = 'http://127.0.0.1:5000';
+const SERVER = 'https://insta-dm-webhooks.herokuapp.com';
+
+const socket = socketClient(SERVER, { transports: ['websocket'] });
+
+socket.on('test', (data) => console.log(data));
 
 const InstaDM = () => {
   const [userAccessToken, setUserAccessToken] = useState('');
@@ -32,14 +40,33 @@ const InstaDM = () => {
     console.log('page access token saved');
   };
 
-  //   geting instagram conversation - message threads
+  // geting instagram conversation - message threads
   const getInstaMessageThreads = async () => {
     const { data } = await axios(
-      `https://graph.facebook.com/v9.0/${userPageID}/conversations?platform=instagram&access_token=${userPageToken}`
+      `https://graph.facebook.com/v9.0/${userPageID}/conversations?platform=instagram&access_token=EAAIEDxV2qx4BACnBbwmIXZBpGJuxW9POeq3m497HZBRD8ZB970Jg9Q1CuIwLqMUvZCFaJ5rgZAMcP1bn17W8NUY0bm152cQWjpgSPXqYZAZAiU7V4vLuv3XvN5ZBBTwH34xpFyQCMujAD25mYY1HkzduzId2HRqCEZCJHhfnAWpymtZC3HZCdf014SnzBPCZCIOL29YtUidndGZAjIQZDZD`
     );
     console.log(data);
     // setUserPageToken(data.data[0].access_token);
     // console.log('page access token saved');
+  };
+
+  //enable page subscriptions
+  const enablePageSubscription = async () => {
+    const { data } = await axios(
+      `https://graph.facebook.com/v12.0/${userPageID}/subscribed_apps?subscribed_fields=feed&access_token=${userPageToken}`
+    );
+    console.log(data);
+    // setUserPageToken(data.data[0].access_token);
+    // console.log('page access token saved');
+  };
+
+  // confirm page subscriptions
+  const confirmPageSubscription = async () => {
+    const { data } = await axios(
+      `https://graph.facebook.com/v12.0/me/subscribed_apps?access_token=${userPageToken}`
+    );
+    console.log(data);
+    console.log('Page susbsccription enabled');
   };
   return (
     <div className='flex flex-col h-full items-center w-full'>
@@ -48,12 +75,14 @@ const InstaDM = () => {
         <FacebookLogin
           appId='994602868049822'
           callback={fbLoginResponse}
-          scope='instagram_basic, instagram_manage_messages, pages_manage_metadata'
+          scope='instagram_basic, instagram_manage_messages, pages_manage_metadata, pages_manage_metadata, pages_show_list'
           render={(renderProps) => <button onClick={renderProps.onClick}>Login with Facebook</button>}
         />
         <button onClick={getUserPages}>Get User Pages</button>
         <button onClick={getUserPageToken}>Get FB Page Access Token</button>
         <button onClick={getInstaMessageThreads}>Get Instagram Message Threads</button>
+        <button onClick={enablePageSubscription}>Enable page subscriptions</button>
+        <button onClick={confirmPageSubscription}>Confirm page subscriptions</button>
       </div>
       <div className='flex items-center justify-center py-4 text-white text-xl w-1/2 bg-purple-600 mt-12'>
         {/* Result */}
